2e3ff75901df8bf5fc0dab8e7de8f0568eb0ccf0
Dhyego Tourinho <131512335+DhyegoTourinho@users.noreply.github.com>
2025-04-02 09:32:09 -0300

    Adicionando projetos com typescript

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,9 +9,26 @@ import { Router } from '@angular/router';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  constructor(private router: Router) { }
-  
-  Redirecionar(rota:string) {
-    this.router.navigate([rota])
+
+  projetos: Projeto[] = [];
+  constructor() {
+    this.projetos.push(this.adicionarProjeto("inegavel", "https://github.com/DhyegoTourinho/FrontEnd-Estudos/tree/main/Projects/Inegavel/inegavel", "ProjetoInegavel.png"));
+    this.projetos.push(this.adicionarProjeto("JogoDasCharadas", "https://github.com/DhyegoTourinho/Projetos/tree/main/JogoDasCharadas", "JogoDasCharadas.png"));
+    //TODO: Adicionar mais projetos.
   }
+
+  adicionarProjeto(Nome:string, Link:string, Imagem:string){
+    let projeto:Projeto = {
+      nome: Nome,
+      link: Link,
+      imagem: "assets/" + Imagem
+    }
+    return projeto;
+  }
+}
+
+class Projeto{
+  nome:string;
+  link:string;
+  imagem:string;
 }
